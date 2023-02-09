6c75100b22ed6113b2c3ede836face2dd3697832
arcenio <arcenio.1701625174@ucaldas.edu.co>
2023-02-09 09:35:17 -0500

    se agrego la funcion sumar en el archivo app.js

--- app.js
+++ app.js
@@ -13,5 +13,17 @@ function restar(num1, num2){
     console.log(`el resultao de la resta es ${resultado}`);
     return resultado;
 }
+/**
+ * The function sumar takes two numbers as parameters and returns the sum of those numbers.
+ * @param num1 - The first number to add.
+ * @param num2 - The second number to add.
+ * @returns The result of the sum of the two numbers.
+ */
+
+function sumar(num1, num2){
+    let resultado=num1+num2;
+    console.log(`el resultao de la suma es ${resultado}`);
+    return resultado;
+}
 
 restar(n1, n2);
